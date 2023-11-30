567ac142fa85bcb0b55208e82a84f11972d30e1c
Wiktor Kopczyński <131811810+Aste21@users.noreply.github.com>
2023-11-30 01:28:27 +0100

    Update MainTest.cpp

--- MainTest.cpp
+++ MainTest.cpp
@@ -11,7 +11,56 @@ int main()
     std::cin >> b;
 
     b(2, 1) = 123;
+#include "TestMatrix.h"
 
+int main()
+{
+    int numberOfFailedTests = 0;
+    try
+    {
+        testInputFromStream();
+        numberOfFailedTests += testCopyConstructor();
+        numberOfFailedTests += testAssignmentOperator();
+        numberOfFailedTests += testAdditionOperator();
+        numberOfFailedTests += testSubstractionOperator();
+        numberOfFailedTests += testMultiplicationOperator();
+        numberOfFailedTests += testComparisonOperators();
+        numberOfFailedTests += testAdditionAssignmentOperators();
+        numberOfFailedTests += testSubstracionAssignmentOperators();
+        numberOfFailedTests += testMultiplicationAssignmentOperators();
+        numberOfFailedTests += testReferenceCounting();
+        numberOfFailedTests += testSingleElementAccess();
+        numberOfFailedTests += testIndexOutOfRangeException();
+        numberOfFailedTests += testWrongInputTypeException();
+        numberOfFailedTests += testDifferentSizesOfMatrixesException();
+        if (numberOfFailedTests > 0)
+        {
+            std::cout << "The tests were unsuccesful! - Number of failed tests: " << numberOfFailedTests;
+        }
+        else
+        {
+            std::cout << "The tests were succesful!";
+        }
+    }
+    catch (const Matrix::DifferentSizesOfMatrixesException &e)
+    {
+        std::cerr << "Error: Different sizes of matrices.\n";
+    }
+    catch (const Matrix::IndexOutOfRangeException &e)
+    {
+        std::cerr << "Error: Index out of range.\n";
+    }
+    catch (const WrongInputTypeException &e)
+    {
+        std::cerr << "Error: Wrong input type.\n";
+    }
+    catch (const Matrix::ReadFromFileErrorException &e)
+    {
+        std::cerr << "Error: Read from file failed.\n";
+    }
+
+    return 0;
+}
     std::cout << "\nMatrix a: \n";
     std::cout << a << std::endl;
     std::cout << "\nMatrix b: \n";
