646ad519f9ac095230541680952130e5895995a9
Wiktor Kopczyński <131811810+Aste21@users.noreply.github.com>
2023-11-30 01:29:48 +0100

    Update MainTest.cpp

--- MainTest.cpp
+++ MainTest.cpp
@@ -1,16 +1,3 @@
-#include "Matrix.h"
-
-int main()
-{
-    
-    Matrix a;
-    Matrix b;
-    std::cout << "Input matrix a: \n";
-    std::cin >> a;
-    std::cout << "\nInput matrix b: \n";
-    std::cin >> b;
-
-    b(2, 1) = 123;
 #include "TestMatrix.h"
 
 int main()
@@ -60,15 +47,4 @@ int main()
     }
 
     return 0;
-}
-    std::cout << "\nMatrix a: \n";
-    std::cout << a << std::endl;
-    std::cout << "\nMatrix b: \n";
-    std::cout << b << std::endl;
-
-    std::cout<< std::endl << std::endl;
-
-    std::cout << a*b;
-
-    
 }
