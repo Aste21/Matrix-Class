8687e4ff04b1bf5929cc8b61a1144fbe14448340
Wiktor Kopczyński <131811810+Aste21@users.noreply.github.com>
2023-11-30 01:29:18 +0100

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -1,42 +1,61 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
+#include <fstream>
 
 #pragma once
 
-class WrongSizeOfMatrixException {  
-    };
+class Matrix
+{
 
-class Matrix {
+    friend Matrix operator+(const Matrix &lhs, const Matrix &rhs);
+    friend Matrix operator-(const Matrix &lhs, const Matrix &rhs);
+    friend Matrix operator*(const Matrix &lhs, const Matrix &rhs);
+    friend bool operator==(const Matrix &lhs, const Matrix &rhs);
+    friend bool operator!=(const Matrix &lhs, const Matrix &rhs);
 
-    friend Matrix operator+(const Matrix& lhs, const Matrix& rhs);
-    friend Matrix operator-(const Matrix& lhs, const Matrix& rhs);
-    friend Matrix operator*(const Matrix& lhs, const Matrix& rhs);
-    friend bool operator==(const Matrix& lhs, const Matrix& rhs);
-    friend bool operator!=(const Matrix& lhs, const Matrix& rhs);
 private:
     struct MatrixData;
-    MatrixData* data;
+    MatrixData *data;
 
 public:
-
     Matrix();
     Matrix(int rows, int cols);
-    Matrix(const Matrix& other);
+    Matrix(const Matrix &other);
     ~Matrix();
 
     int getRows() const;
     int getColumns() const;
+    int getReferenceCount() const;
 
-    Matrix& operator=(const Matrix& other);
-    Matrix& operator+=(const Matrix& other);
-    Matrix& operator-=(const Matrix& other);
-    Matrix& operator*=(const Matrix& other);
+    Matrix &operator=(const Matrix &other);
+    Matrix &operator+=(const Matrix &other);
+    Matrix &operator-=(const Matrix &other);
+    Matrix &operator*=(const Matrix &other);
 
-    friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
-    friend std::istream& operator>>(std::istream& is, Matrix& matrix);
+    friend std::ostream &operator<<(std::ostream &os, const Matrix &matrix);
+    friend std::istream &operator>>(std::istream &is, Matrix &matrix);
+
+    Matrix &readMatrixFromFile(const std::string fileName);
 
     int operator()(int row, int col) const;
-    int& operator()(int row, int col);
+    int &operator()(int row, int col);
 
-private:
+    void checkSizeOfMatrix(const Matrix &other) const;
+    void checkSizeOfMatrixMultiplication(const Matrix &other) const;
+    void checkIndex(int col, int row) const;
+
+    class DifferentSizesOfMatrixesException
+    {
+    };
+    class IndexOutOfRangeException
+    {
+    };
+    class ReadFromFileErrorException
+    {
+    };
+};
+
+class WrongInputTypeException
+{
 };
