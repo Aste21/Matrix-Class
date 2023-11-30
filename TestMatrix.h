e1f9087f123245121a5ebc9501f8c1d9bb91e514
Wiktor Kopczyński <131811810+Aste21@users.noreply.github.com>
2023-11-30 01:28:49 +0100

    Update TestMatrix.h

---
+++ TestMatrix.h
@@ -0,0 +1,31 @@
+#include "Matrix.h"
+
+void testInputFromStream();
+
+int testCopyConstructor();
+
+int testAssignmentOperator();
+
+int testAdditionOperator();
+
+int testSubstractionOperator();
+
+int testMultiplicationOperator();
+
+int testComparisonOperators();
+
+int testAdditionAssignmentOperators();
+
+int testSubstracionAssignmentOperators();
+
+int testMultiplicationAssignmentOperators();
+
+int testReferenceCounting();
+
+int testSingleElementAccess();
+
+int testIndexOutOfRangeException();
+
+int testWrongInputTypeException();
+
+int testDifferentSizesOfMatrixesException();
